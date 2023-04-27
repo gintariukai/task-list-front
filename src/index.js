271d1c1ed2ddec46fe7edd6ea55ff6ac5039b96a
gintariukai <gintariukai@hotmail.com>
2023-04-27 12:07:54 +0300

    22: Create a Context for the Current User

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import {CurrentUserProvider} from "contexts/currentUser";
 import Routes from "pages/routes";
 import TopBar from "components/topBar";
 
 const App = ()  => {
     return (
-        <div>
+        <CurrentUserProvider>
             <Router>
                 <TopBar />
                 <Routes />
             </Router>
-        </div>
+        </CurrentUserProvider>
     )
 }
 
